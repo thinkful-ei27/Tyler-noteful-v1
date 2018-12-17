304437962699f62c2072e173fb1c173e658bf1db
Tyler Crabb <tydacrabb@gmail.com>
2018-12-17 11:43:19 -0800

    step one

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict';
 
 // Load array of notes
-const data = require('./db/notes');
+
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const data = require('./db/notes');
+const app = express();
+
+// add static server
+
+app.listen(8080, function() {
+  console.info(`server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
