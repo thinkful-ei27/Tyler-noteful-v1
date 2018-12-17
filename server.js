6da14eadd39edfdcc8d0e65b0ff9ad4780cd4bb3
Tyler Crabb <tydacrabb@gmail.com>
2018-12-17 11:57:11 -0800

    step 2

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ const express = require('express');
 const data = require('./db/notes');
 const app = express();
 
+app.use(express.static('public'));
 // add static server
 
 app.listen(8080, function() {
   console.info(`server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
+});
+app.get
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
 });
